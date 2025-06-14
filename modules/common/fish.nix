6c0d7c300dedd53e0984a7bbad5dce877f26720f
Marvin Countryman <me@maar.vin>
2025-06-13 20:34:23 -0400

    Fix all users getting fish shell

--- modules/common/fish.nix
+++ modules/common/fish.nix
@@ -3,5 +3,5 @@
   # Enable alternative shell support in nix-darwin.
   programs.fish.enable = true;
   # Set the user's login shell
-  users.users = builtins.mapAttrs (_: _: { shell = "${pkgs.fish}/bin/fish"; }) config.users;
+  users.users = builtins.mapAttrs (_: _: { shell = "${pkgs.fish}/bin/fish"; }) config.dotfiles.users;
 }
