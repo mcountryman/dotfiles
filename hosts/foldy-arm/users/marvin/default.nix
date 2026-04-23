24afdb8661b348512b86010d9567e76a6c830ed2
Marvin Countryman <me@maar.vin>
2026-04-23 08:47:59 -0400

    Add gpg agent reset on fish shell init

--- hosts/foldy-arm/users/marvin/default.nix
+++ hosts/foldy-arm/users/marvin/default.nix
@@ -5,6 +5,7 @@
 
   # YubiKey host: override SSH auth to use gpg-agent socket
   programs.fish.interactiveShellInit = lib.mkAfter ''
+    gpg-connect-agent /bye
     set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
   '';
 }
