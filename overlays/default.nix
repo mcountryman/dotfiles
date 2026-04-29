0dd231b868b3ed88364ce394b4fb140f4a4cf7f4
Marvin Countryman <me@maar.vin>
2026-04-29 11:27:51 -0400

    revert: remove zellij 0.44.1 overlay, stay on nixpkgs 0.43.1

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,17 +8,4 @@
 nixpkgs.lib.composeManyExtensions [
   helix.overlays.default
   llm-agents.overlays.shared-nixpkgs
-  (_final: prev: {
-    zellij = prev.zellij.overrideAttrs (_old: rec {
-      version = "0.44.1";
-      src = prev.fetchFromGitHub {
-        owner = "zellij-org";
-        repo = "zellij";
-        rev = "v${version}";
-        hash = "sha256-KHpVUjuOmMtkt8qBaCozD3M44eEtDwFmdDfszKAz0bM=";
-      };
-      cargoHash = "";
-      cargoLock.lockFile = "${src}/Cargo.lock";
-    });
-  })
 ]
