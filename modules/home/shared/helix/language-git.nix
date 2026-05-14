ae3a885f4869324ac98eceeb42eace5a04fe7861
Marvin Countryman <me@maar.vin>
2026-05-13 20:15:19 -0400

    feat(helix): add explicit git-commit rulers

--- modules/home/shared/helix/language-git.nix
+++ modules/home/shared/helix/language-git.nix
@@ -3,8 +3,13 @@
     language = [
       {
         name = "git-commit";
+        text-width = 72;
         auto-format = true;
         language-servers = [ "spellcheck" ];
+        rulers = [
+          50
+          72
+        ];
       }
     ];
   };
