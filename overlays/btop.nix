ceb28304eca167c52b88bc0464253a272eb68870
Marvin Countryman <me@maar.vin>
2026-05-13 20:01:17 -0400

    fix(overlays): btop missing src
    - fix overlay nag about a missing `src`
    - reset the nativeInstallCheckInputs to fix the missing version info
    build error

--- overlays/btop.nix
+++ overlays/btop.nix
@@ -1,5 +1,14 @@
 _final: prev: {
-  btop = prev.btop.overrideAttrs {
+  btop = prev.btop.overrideAttrs rec {
     version = "1.4.7";
+    src = prev.fetchFromGitHub {
+      owner = "aristocratos";
+      repo = "btop";
+      tag = "v${version}";
+      hash = "sha256-ZLT+Hc1rvBFyhey+imbgGzSH/QaVxIh/jvDKVSmDrA0=";
+    };
+
+    # Remove version check hook
+    nativeInstallCheckInputs = [ ];
   };
 }
